d3970ef8ef17039ff58651f6d06daa8dae33eea7
elvinmwarangu-stack <elvin.mwarangu@student.moringaschool.com>
2025-09-10 07:14:55 +0300

    added a file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
